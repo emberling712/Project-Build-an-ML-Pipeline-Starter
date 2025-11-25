afa6822031ce5fa9bef86653ff52654b717c43d2
emberling712 <krabey1@wgu.edu>
2025-11-25 14:09:16 -0800

    datacheck try one

--- main.py
+++ main.py
@@ -65,10 +65,16 @@ def go(config: DictConfig):
 
 
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "data_check")
+                "main",
+                parameters={
+                    "csv": "clean_sample.csv:reference",
+                    "ref": "clean_sample.csv:reference",
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"],
+                },
+            )
 
         if "data_split" in active_steps:
             ##################
